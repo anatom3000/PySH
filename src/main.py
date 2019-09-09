8e8f86dfe0af3e501eaa501654b441bc11c8117f
anat3000 <49984698+anat3000@users.noreply.github.com>
2019-09-09 13:49:55 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,22 +6,9 @@ from getpass import getpass
 
 command = ""
 
-verrouille = True
-while verrouille:
-  
-  pswd = getpass("Type password : ")
-  entre = entre.encode()
-    
-  entre_chiffre = hashlib.sha1(entre).hexdigest()
-  if entre_chiffre == mot_de_passe_chiffre:
-    verrouille = False
-  else:
-    print("Incorrect password.")
-        
-print("Mot de passe accepté."
-
 print("PySh v0.1 - A simple and portable Shell in Python and for Python.\nType \"help <command>\" for help.")
 while command is not "exit":
-  
+  //nothing now
+  pass
 
 sys.exit(0)
